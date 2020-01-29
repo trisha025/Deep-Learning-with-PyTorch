0885cdd85356dfb713511fbdfb416fa36880a8da
Trisha <singhaltrisha25@gmail.com>
2020-01-29 20:26:12 +0800

    pytorch tensors for DL

--- Creating_PyTorch_Tensors.py
+++ Creating_PyTorch_Tensors.py
@@ -25,3 +25,26 @@ print((torch.tensor(np.array([1,2,3]))).dtype)      #int as input and int as out
 print(torch.tensor(np.array([1., 2., 3.])))     #float as input and float as output
 
 print(torch.tensor(np.array([1,2,3]), dtype = torch.float64))       #explicity setting data type
+
+#Memory Sharing vs Copying
+
+data = np.array([1,2,3])
+print(data)
+
+t1 = torch.Tensor(data)
+t2 = torch.tensor(data)
+t3 = torch.as_tensor(data)          #factor function
+t4 = torch.from_numpy(data)
+
+data[0] = 0
+data[1] = 0
+data[2] = 0
+
+#creating additional copy of input data in memory
+print(t1) 
+print(t2)
+
+#share data in memory
+print(t3)
+print(t4)
+
