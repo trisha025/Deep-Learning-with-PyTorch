7a3cf467be108aea05e4e8554e3005e002eb9666
Trisha <singhaltrisha25@gmail.com>
2020-02-01 20:42:11 +0800

    ETL_edit

--- Fashion_MNIST_Project.py
+++ Fashion_MNIST_Project.py
@@ -1,1 +1,18 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+
+#download data
+train_set = torchvision.datasets.FashionMNIST(
+    root = './data/FashionMNIST',
+    train=True,
+    download=True,
+    transform=transforms.Compose([
+        transforms.ToTensor()
+    
+    ])
+)
+
+#data loading into an object
+train_loader = torch.utils.data.DataLoader(train_set)
+
