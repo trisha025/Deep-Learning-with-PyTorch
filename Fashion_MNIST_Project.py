3d6b2a6354055612dea2a2a1f70394c71dd121d2
Trisha <singhaltrisha25@gmail.com>
2020-02-02 16:32:02 +0800

    fashion mnist

--- Fashion_MNIST_Project.py
+++ Fashion_MNIST_Project.py
@@ -1,6 +1,10 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+torch.set_printoptions(linewidth=120)
 
 #download data
 train_set = torchvision.datasets.FashionMNIST(
@@ -14,5 +18,9 @@ train_set = torchvision.datasets.FashionMNIST(
 )
 
 #data loading into an object
-train_loader = torch.utils.data.DataLoader(train_set)
+train_loader = torch.utils.data.DataLoader(
+    train_set, batch_size=10)
 
+print("Total number of traning images: ", len(train_set))
+
+print("Training labels: ", train_set.targets)
