6543f77c94fa60fd7157c7a6e1423743ae5f2de3
Trisha <singhaltrisha25@gmail.com>
2020-02-02 17:32:02 +0800

    fashion

--- Fashion_MNIST_Project.py
+++ Fashion_MNIST_Project.py
@@ -24,3 +24,30 @@ train_loader = torch.utils.data.DataLoader(
 print("Total number of traning images: ", len(train_set))
 
 print("Training labels: ", train_set.targets)
+
+print("Number of images in each class: ", train_set.targets.bincount())
+
+#to access an individual sample
+sample = next(iter(train_set))
+print(len(sample)) # =2 (i) image (ii) label
+
+print(type(sample))
+image, label = sample
+
+#sample shape
+print(image.shape)
+
+plt.imshow(image.squeeze(), cmap = 'gray')
+print('label: ', label)
+plt.show()
+
+#to access a batch
+batch = next(iter(train_loader))
+
+print(len(batch))
+print(type(batch))
+images, labels = batch
+
+#batch shape
+print(images.shape)
+print(labels.shape)
